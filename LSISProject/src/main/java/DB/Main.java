6f5ccfc9d05fbfc010e8372558e6d128c8911948
Margarida Mendes <margarida.arm@gmail.com>
2022-06-15 12:31:58 +0100

    Project configurations
    pom.xml igual ao projeto modelo do prof.

--- LSISProject/src/main/java/DB/Main.java
+++ LSISProject/src/main/java/DB/Main.java
@@ -1,13 +1,13 @@
 package DB;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.file.FileSystemOptions;
 
 public class Main {
 
     public static void main(String[] args) {
-        FileSystemOptions fsOptions = new FileSystemOptions();
         Vertx vertx = Vertx.vertx();
+
+        vertx.deployVerticle(new VerticleRSJson());
 
     }
 }
